60e6484f1a1bb59329a783d459a9df45b9c9d64d
bluemelodia <hsumelanie@yahoo.com>
2015-11-01 15:31:06 -0500

    Added integrator method that supports requester POST/DELETE on own keys.

--- api/integrator.py
+++ api/integrator.py
@@ -68,6 +68,11 @@ def response(data, code):
 	resp = Response(js, status = code, mimetype='application/json')
 	return resp
 
+# Log message format: <timestamp> <requester IP> [<pkeys>] [<fkeys> IF EXISTS] <CRUD operation>
+# Ex. Steve Jobs leaves UNI: 
+#	2015-11-01T15:26:22.729117 127.0.0.1 [Steve Jobs] DELETE
+# Ex. COMS4111-3 is cancelled
+#	2015-11-01T15:27:21.546925 127.0.0.1 [COMS4111-3] DELETE
 def writeToLog(message):
 	print "Written to log"
 
@@ -77,15 +82,17 @@ def writeToLog(message):
 # Cases where students will call integrator:
 #	Student adds a class
 #	Student deletes a class
-#	Student leaves the university
+#	Student leaves the university (dropping all classes)
 #	Student changes their name (must provide both first and last name)
+#	Student updates current coureses
+#	Student updates past courses
 
-# POST with primary key only
+# POST with key only (primary keys are cid or uid), supports requester POST & DELETE only!
 # To call: curl -X POST http://127.0.0.1:9001/integrator/<pkeys_separated_by_underlines>/<CRUD op>
 # Ex: curl -X POST http://127.0.0.1:9001/integrator/Steve_Jobs/DELETE where Steve Jobs leaves uni
 #	  curl -X POST http://127.0.0.1:9001/integrator/COMS4111-3/DELETE where COMS4111-3 is cancelled
-@app.route('/integrator/<pkey>/<action>', methods = ['POST'])
-def post_pkey(pkey, action):
+@app.route('/integrator/<key>/<action>', methods = ['POST'])
+def post_pkey(key, action):
 	ip = request.remote_addr #save requester's IP address
 	if (checkIP(ip)):
 		if (not checkAction(action)): #make sure requester specified a CRUD operation
@@ -94,7 +101,22 @@ def post_pkey(pkey, action):
 	else:
 		data = {'error':'Unknown requester IP address'}
 		return response(data, 403)
-	
+	# Construct the message to log
+	message = str(datetime.datetime.now().isoformat()) + " " + str(ip) + " ["
+	split_key = key.replace("_", " ")
+	for i in range(len(split_key)):
+		message += split_key[i]
+	message += "] " + str(action)
+
+	# Log the message
+	writeToLog(message)
+
+	# Return the logged message to the requester
+	data = {'logged':message}
+	return response(data, 200)
+
+# POST with key only, supports requester PUT
+@app.route('/integrator/<key_oldval>/<key_newval>/<action>', methods = ['POST'])
 
 
 # POST with primary & foreign key
