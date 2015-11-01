36189f4b4e46a89e5180d43ead2f681fac814a48
bluemelodia <hsumelanie@yahoo.com>
2015-10-31 21:46:08 -0400

    Can send GET and POST requests to the integrator now.

--- api/integrator.py
+++ api/integrator.py
@@ -1,4 +1,16 @@
 import sys
+import datetime
+import pprint
+
+from bson.json_util import dumps
+
+# Import and initialize Flask
+from flask import Flask, url_for
+from flask import request
+from flask import json
+from flask import Response
+from flask import jsonify
+app = Flask(__name__)
 
 # python integrator.py <url for courses> <IP for courses> <number of student partitions> 
 # <list of student paritions followed by their IP>
@@ -13,16 +25,26 @@ def main(argv):
 		print "Too few command-line arguments."
 		sys.exit(1)
 	courses = argv[0]
-	print courses
 	coursesIP = argv[1]
 	numberOfPartitions = int(argv[2])
 	argNumber = 3
 	for i in range(numberOfPartitions):
 		students[argv[argNumber]] = argv[argNumber+1]
 		argNumber += 2
-	print students
 
-#integrator 
+	app.run(
+		host = "localhost",
+		port = int("9000")
+	)
+
+@app.route('/integrator', methods = ['GET', 'POST'])
+def integrator():
+	if request.method == 'POST':
+		print "POST"
+	else:
+		print "GET"
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
+
+#python integrator.py courses course123 2 students students111 students2 students222
