c50b95fe31c21fa6ce777dcdd6cd933c8c83adac
bluemelodia <hsumelanie@yahoo.com>
2015-11-07 15:08:59 -0500

    Integrator will now add a forward:false message.

--- api/integrator.py
+++ api/integrator.py
@@ -193,7 +193,7 @@ def post_key_POST_OR_DEL(primary_key):
 					print "Students added " + str(uid) + " to class " + str(cid)
 					url = courses + "courses/" + cid + "/students"
 					print "SENT URL: " + url
-					res = requests.post(url, json={'uid':uid})
+					res = requests.post(url, json={'uid':uid, 'forward':false})
 					print "Notified courses that " + str(uid) + " added class " + str(cid)
 					print "Response from courses: " + res.text
 				else: # that class does not exist, undo student's action
