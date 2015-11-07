b9e01e9e6f15cd9065215d95e3dbfc9dba0bd0fe
bluemelodia <hsumelanie@yahoo.com>
2015-11-07 16:57:43 -0500

    Capitalized courses.

--- api/courses.py
+++ api/courses.py
@@ -180,7 +180,7 @@ def not_found(error=None):
 # Looks for forward: false tag from integrator requests
 def do_not_forward():
     data = form_or_json()
-    return true if 'forward' in data else false
+    return True if 'forward' in data else False
 
 # Returns data whether from request.form or request.data
 def form_or_json():
