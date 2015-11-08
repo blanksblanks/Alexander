356ff7491580a0f2ae81d3b67bf597aab822e6b3
bluemelodia <hsumelanie@yahoo.com>
2015-11-08 00:25:46 -0500

    JSON parse issue in courses delete.

--- api/integrator.py
+++ api/integrator.py
@@ -205,18 +205,15 @@ def post_key_POST_OR_DEL(primary_key):
 				url = "http://127.0.0.1:" + str(courses_port) + "/courses/" + cid + "/students/" + uid
 				payload = json.dumps({"uid":uid, "forward":"False"})
 				res = requests.delete(url, data=payload)
-			else: # remove poppy from all classes
-				print "other deletion"
+			else: # remove student from all classes
 				old_dictionary = eval(old_record)
 				cid_list = old_dictionary["cid_list"]
 				for v in cid_list:
 					url = "http://127.0.0.1:" + str(courses_port) + "/courses/" + str(v) + "/students/" + uid
 					payload = json.dumps({"uid":uid, "forward":"False"})
 					res = requests.delete(url, data=payload)
-				#cid_list = old_record["cid_list"] 
-				#/courses/<cid>/students/<uid>
-				#print cid_list
 		else:
+			print old_record
 			for k, v in students.iteritems():
 				url = "http://127.0.0.1:" + str(int(v)) + "/students/" + uid + "/courses/" + cid
 				payload = json.dumps({"cid":cid, "forward":"False"})
